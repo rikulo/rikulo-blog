5de0287ecf842f8c5c8de1ef76145bd57d3f35dd
Timothy Clare <timothyclare@zkoss.org>
2012-06-22 09:43:29 +0800

    Initial blog upload with starter configuration & Makefile

---
+++ pelican.conf.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+
+AUTHOR = u"Various"
+SITENAME = u"Rikulo Blog"
+SITEURL = '/'
+
+TIMEZONE = 'Europe/Paris'
+
+DEFAULT_LANG='en'
+
+# Blogroll
+LINKS =  (
+    ('Pelican', 'http://docs.notmyidea.org/alexis/pelican/'),
+    ('Python.org', 'http://python.org'),
+    ('Jinja2', 'http://jinja.pocoo.org'),
+    ('You can modify those links in your config file', '#')
+         )
+
+# Social widget
+SOCIAL = (
+          ('You can add links in your config file', '#'),
+         )
+
+DEFAULT_PAGINATION = 7
+
+
+    
